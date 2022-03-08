0578fd67c7cb86a705ae3715db2d385a41eb92c7
Justin <56172862+Ng-Cho-Yin@users.noreply.github.com>
2022-03-08 18:51:41 +0800

    Update finance.py

--- finance.py
+++ finance.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+def data_clean(x):
+    if "." and "," in x:
+        x = x.replace(".","")
+        x = x.replace(",",".")
+        return x
+    else:
+        return x.replace(",",".")
 
 def finance():
     
@@ -29,17 +36,41 @@ def finance():
                    'other transaction fees','other','total']
     
     uploaded_file = st.file_uploader('交易记录上传口')
-    if uploaded_file is not None:
-                uploaded_file = pd.read_csv(uploaded_file, skiprows=7)
+            if uploaded_file is not None:
+                uploaded_file = pd.read_csv(uploaded_file, skiprows=7, encoding = "ISO-8859-1")
+
+                if 'account type' in uploaded_file:
+                    st.write(1)
+                    uploaded_file = uploaded_file.drop('account type', 1)
                 uploaded_file.columns = cat
+                try:
+
+                    uploaded_file['product sales'] = uploaded_file['product sales'].apply(data_clean)
+                    uploaded_file['product sales tax'] = uploaded_file['product sales tax'].apply(data_clean)
+                    uploaded_file['shipping credits'] = uploaded_file['shipping credits'].apply(data_clean)
+                    uploaded_file['shipping credits tax'] = uploaded_file['product sales'].apply(data_clean)
+                    uploaded_file['gift wrap credits'] = uploaded_file['gift wrap credits'].apply(data_clean)
+                    uploaded_file['giftwrap credits tax'] = uploaded_file['giftwrap credits tax'].apply(data_clean)
+                    uploaded_file['promotional rebates'] = uploaded_file['promotional rebates'].apply(data_clean)
+                    uploaded_file['promotional rebates tax'] = uploaded_file['promotional rebates tax'].apply(data_clean)
+                    uploaded_file['marketplace withheld tax'] = uploaded_file['marketplace withheld tax'].apply(data_clean)
+                    uploaded_file['selling fees'] = uploaded_file['selling fees'].apply(data_clean)
+                    uploaded_file['fba fees'] = uploaded_file['fba fees'].apply(data_clean)
+                    uploaded_file['other transaction fees'] = uploaded_file['other transaction fees'].apply(data_clean)
+                    uploaded_file['other'] = uploaded_file['other'].apply(data_clean)
+                    uploaded_file['total'] = uploaded_file['total'].apply(data_clean)
+
+
+                except:
+                    pass
+
                 uploaded_file.to_csv('logs2.csv')
                 res = {instanceNames[i]: instanceNames_Chi[i] for i in range(len(instanceNames))}
                 holder = {name: Create_objs(name=name, datafile='logs2.csv') for name in instanceNames}
-
                 st.write(' ## 销售额:$', float(round(holder['product sales'].sum)))
                 platfrom_fees = holder['marketplace withheld tax'].sum + holder['selling fees'].sum + holder[
                     'fba fees'].sum + holder['other transaction fees'].sum + holder['other'].sum
-                st.write(' ## 平台费用:$', platfrom_fees)
+                st.write(' ## 平台费用:$', round(platfrom_fees))
                 st.write(' ## 利润:$', float(round(holder['total'].sum)))
                 col1, col2, col3, col4, col5 = st.columns((1, 1, 1, 1, 1))
                 h = 1
