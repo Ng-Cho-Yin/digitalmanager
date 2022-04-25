40766f0f85b42242d4770aa70b582ffdeb0e22bb
Justin <56172862+Ng-Cho-Yin@users.noreply.github.com>
2022-04-25 21:07:42 +0800

    Update warehouse.py

--- warehouse.py
+++ warehouse.py
@@ -1,12 +1,15 @@
 import streamlit as st
 from backend import download
-
+from backend import upload
+from backend import delete
 from backend import iterator
-
+from backend import openfile
 from backend import table
 from backend import render_pie_simple
 import pandas as pd
 import os.path
+import base64
+import time
 
 
 
@@ -52,32 +55,43 @@ def warehouse():
     col_pos = 1
     data = []
     for sku in target_col:
+        # st.metric(label=sku, value=int(df.at[house_pos, sku]))
         if col_pos == 1:
             col1.metric(label=sku, value=int(df.at[house_pos, sku]))
-
             Dict = {"value": int(df.at[house_pos, sku]), "name": sku}
             data.append(Dict)
+
 
         elif col_pos == 2:
             col2.metric(label=sku, value=int(df.at[house_pos, sku]))
             Dict = {"value": int(df.at[house_pos, sku]), "name": sku}
             data.append(Dict)
 
+
         elif col_pos == 3:
             col3.metric(label=sku, value=int(df.at[house_pos, sku]))
             Dict = {"value": int(df.at[house_pos, sku]), "name": sku}
             data.append(Dict)
 
+
         elif col_pos == 4:
             col4.metric(label=sku, value=int(df.at[house_pos, sku]))
             Dict = {"value": int(df.at[house_pos, sku]), "name": sku}
             data.append(Dict)
-
+            col_pos = 0
         col_pos += 1
 
     render_pie_simple('统计1', '统计2', '统计3', data)
 
-    st.write('## ')
+    path = '仓库出入库统计' + '/' + '仓库出入库统计.csv'
+    download(buckets[1], str(path), '仓库出入库统计.csv')
+    df = pd.read_csv('仓库出入库统计.csv')
+    try:
+        df.drop('Unnamed: 0', axis=1, inplace=True)
+    except:
+        pass
+    table(df, df.columns, '仓库出入库统计.csv', False, height=500)
+    os.remove('仓库出入库统计.csv')
 
     with st.expander('仓库详细资料', expanded=True):
         if st.checkbox('查看各仓库的类型,名称,国家'):
@@ -90,14 +104,4 @@ def warehouse():
                 pass
             table(df, df.columns, '仓库资料.csv', False, height=500)
             os.remove('仓库资料.csv')
-    with st.expander('仓库出入库统计', expanded=True):
-        if st.checkbox('查看所以仓库各产品的现存量'):
-            path = '仓库出入库统计' + '/' + '仓库出入库统计.csv'
-            download(buckets[1], str(path), '仓库出入库统计.csv')
-            df = pd.read_csv('仓库出入库统计.csv')
-            try:
-                df.drop('Unnamed: 0', axis=1, inplace=True)
-            except:
-                pass
-            table(df, df.columns, '仓库出入库统计.csv', False, height=500)
-            os.remove('仓库出入库统计.csv')
+
